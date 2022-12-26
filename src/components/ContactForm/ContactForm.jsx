ab0eb177a2ef3cf1209f41e21dffd6d2a6bda566
Olga V <ol0108ha@gmail.com>
2022-12-26 17:54:58 +0200

    update-form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class ContactForm extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+    nameInputId: nanoid(),
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState({ name: '', number: '' });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="name">
+            <p>Name</p>
+          </label>
+
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            id={this.nameInputId}
+            value={name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label>
+            <p>Number</p>
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={this.handleChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
